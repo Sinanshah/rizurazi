2090fb0a0dba579e7fb0a803db0dbad561f206e4
Sinanshah <91791589+Sinanshah@users.noreply.github.com>
2021-10-06 12:08:50 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('💥 SORRY💥', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('', url=f'{TUTORIAL}')
                     ]
                     ]
                 await bot.send_cached_media(
